4b1cb3e8601c3c6ea93d822d88eba7109d97d33c
monofon <monofon@tramberend.de>
2020-08-11 17:06:49 +0200

    Add DECKER_BASE_URL environment variable

--- app/Main.hs
+++ app/Main.hs
@@ -14,6 +14,7 @@ import Relude
 import Servant
 import Servant.JS
 import Servant.JS.Vanilla
+import System.Environment
 
 import API
 import Model
@@ -23,28 +24,33 @@ app = serve deckerAPI deckerServer
 
 main :: IO ()
 main = do
+  baseUrl <- toText . fromMaybe "" <$> lookupEnv "DECKER_BASE_URL"
   putStrLn "# decker-engine"
-  Text.writeFile "static/decker.js" $ jsForAPI jsAPI vanillaJS
-  now <- getCurrentTime
-  runSqlite "db/engine.db" $ do
-    runMigration migrateAll
-    p1 <- insert $ Person "person1"
-    p2 <- insert $ Person "person2"
-    insert $
-      Comment
-        "What is the purpose of this stuff? Nobody ever needs to know this."
-        "cgg"
-        "intro-slide"
-        (Just p1)
-        now
-    insert $
-      Comment "May I go to the bathroom?" "cgg" "intro-slide" (Just p1) now
-    insert $ Comment "Wat?" "cgg" "intro-slide" (Just p2) now
-    insert $
-      Comment
-        "Fucking hell, this is really anonymous! Keep on swearing."
-        "cgg"
-        "slide-2"
-        Nothing
-        now
+  Text.writeFile "static/decker.js" $
+    jsForAPI
+      jsAPI
+      (vanillaJSWith defCommonGeneratorOptions {urlPrefix = baseUrl})
+  runSqlite "db/engine.db" $ do runMigration migrateAll
   run 8081 app
+
+mock :: ReaderT SqlBackend IO (Key Comment)
+mock = do
+  now <- liftIO getCurrentTime
+  p1 <- insert $ Person "person1"
+  p2 <- insert $ Person "person2"
+  insert $
+    Comment
+      "What is the purpose of this stuff? Nobody ever needs to know this."
+      "cgg"
+      "intro-slide"
+      (Just p1)
+      now
+  insert $ Comment "May I go to the bathroom?" "cgg" "intro-slide" (Just p1) now
+  insert $ Comment "Wat?" "cgg" "intro-slide" (Just p2) now
+  insert $
+    Comment
+      "Fucking hell, this is really anonymous! Keep on swearing."
+      "cgg"
+      "slide-2"
+      Nothing
+      now
--- makefile
+++ makefile
@@ -1,9 +1,26 @@
+decker-engine := ~/.local/bin/decker-engine
+
+directory := $(PWD)
+pidfile := decker-engine.pid
+lockfile := decker-engine.lock
+
 build:
 	stack build
 
-fill:
-	curl -X POST -d "First comment!" -H 'Accept: application/json' -H 'Content-type: application/json' http://localhost:8081/comments/cgg-1/intro/henrik
+open: build
+	open -a safari http://localhost:8081/static/index.html
+	sleep 1
+	stack run -- decker-engine
 
-list:
-	curl -H 'Accept: application/json' -H 'Content-type: application/json' http://localhost:8081/comments
+install-service: build
+	sudo cp decker-engine.service /etc/systemd/system/
+	sudo chmod 644 /etc/systemd/system/decker-engine.service
+	sudo systemctl daemon-reload
 
+daemon: build kill
+	daemonize -c $(directory) -p $(pidfile) -l $(lockfile) $(decker-engine)
+
+kill:
+	if [ -f $(pidfile) ]; then \
+		kill `cat $(pidfile)` && rm -f decker-engine.lock decker-engine.pid; \
+	fi
