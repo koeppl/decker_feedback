88886e7d51200fc51a758988722b0483748f9530
monofon <monofon@tramberend.de>
2020-08-12 18:49:13 +0200

    Extract useful functions

---
+++ api/decker-util.js
@@ -0,0 +1,74 @@
+function submitComment(getContext, container, textarea) {
+  let context = getContext();
+  if (!textarea.value) {
+    return;
+  }
+
+  postCommentsByDeckBySlideByAuthor(
+    context.deck,
+    context.slide,
+    context.token,
+    textarea.value,
+    _ => {
+      textarea.value = "";
+      updateCommentList(getContext, container);
+    },
+    err => {
+      console.log(err);
+    }
+  );
+}
+
+function updateCommentList(getContext, container) {
+  let context = getContext();
+  getCommentsByDeckBySlide(
+    context.deck,
+    context.slide,
+    list => {
+      fillContainer(getContext, container, list);
+    },
+    err => {
+      console.log(err);
+    }
+  );
+}
+
+function fillContainer(getContext, container, list) {
+  let context = getContext();
+  while (container.firstChild) {
+    container.removeChild(container.lastChild);
+  }
+  for (let [key, comment] of list) {
+    let div = document.createElement("div");
+    div.textContent = comment.markdown;
+    if (comment.author) {
+      getAuthorsById(
+        comment.author,
+        author => {
+          if (author.token === context.token) {
+            let del = document.createElement("button");
+            del.textContent = "✖";
+            del.addEventListener("click", _ => {
+              deleteCommentsById(
+                key,
+                _ => {
+                  updateCommentList(getContext, container);
+                },
+                err => {
+                  console.log(err);
+                }
+              );
+            });
+            div.appendChild(del);
+          }
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    }
+    container.appendChild(div);
+  }
+  container.scrollTop = 0;
+}
+
--- api/test.js
+++ api/test.js
@@ -1,122 +1,48 @@
-console.log("You're on!");
-
 window.addEventListener("load", e => {
   let deckid = document.getElementById("deckid");
   let slideid = document.getElementById("slideid");
   let token = document.getElementById("persontoken");
+
+  let container = document.getElementById("comment-list-1");
   let update = document.getElementById("update-button");
+
   let textarea = document.getElementById("add-comment-1");
   let submit = document.getElementById("submit-button");
 
+  let getContext = () => {
+    return {
+      deck: deckid.value,
+      slide: slideid.value,
+      token: token.value
+    };
+  };
+
   deckid.addEventListener("keydown", e => {
-    if (e.key === "Enter") updateCommentList();
+    if (e.key === "Enter") updateCommentList(getContext, container);
   });
 
   slideid.addEventListener("keydown", e => {
-    if (e.key === "Enter") updateCommentList();
+    if (e.key === "Enter") updateCommentList(getContext, container);
   });
 
   token.addEventListener("keydown", e => {
-    if (e.key === "Enter") updateCommentList();
+    if (e.key === "Enter") updateCommentList(getContext, container);
   });
 
-  update.addEventListener("click", e => {
-    updateCommentList();
+  update.addEventListener("click", _ => {
+    updateCommentList(getContext, container);
   });
 
   textarea.addEventListener("keydown", e => {
     if (e.key === "Enter" && e.shiftKey) {
-      submitComment();
-      updateCommentList();
+      submitComment(getContext, container, textarea);
     }
   });
 
-  submit.addEventListener("click", e => {
-    submitComment();
-    updateCommentList();
+  submit.addEventListener("click", _ => {
+    submitComment(getContext, container, textarea);
   });
 
-  updateCommentList();
+  updateCommentList(getContext, container);
 });
 
-function submitComment() {
-  let deckid = document.getElementById("deckid").value;
-  let slideid = document.getElementById("slideid").value;
-  let token = document.getElementById("persontoken").value;
-  let textarea = document.getElementById("add-comment-1");
-
-  if (!textarea.value) {
-    return;
-  }
-
-  postCommentsByDeckBySlideByAuthor(
-    deckid,
-    slideid,
-    token,
-    textarea.value,
-    list => {
-      textarea.value = "";
-      updateCommentList();
-    },
-    err => {
-      console.log(err);
-    }
-  );
-}
-
-function updateCommentList() {
-  let deckid = document.getElementById("deckid").value;
-  let slideid = document.getElementById("slideid").value;
-  let token = document.getElementById("persontoken").value;
-  let submit = document.getElementById("submit-button");
-
-  let container = document.getElementById("comment-list-1");
-
-  getCommentsByDeckBySlide(
-    deckid,
-    slideid,
-    list => {
-      fillContainer(container, token, list);
-    },
-    err => {
-      console.log(err);
-    }
-  );
-}
-
-function fillContainer(container, token, list) {
-  while (container.firstChild) {
-    container.removeChild(container.lastChild);
-  }
-  for (let [key, comment] of list) {
-    let div = document.createElement("div");
-    div.textContent = comment.markdown;
-    getAuthorsById(
-      comment.author,
-      author => {
-        if (author.token === token) {
-          let del = document.createElement("button");
-          del.textContent = "✖";
-          del.addEventListener("click", e => {
-            console.log(key);
-            deleteCommentsById(
-              key,
-              s => {
-                updateCommentList();
-              },
-              e => {
-                console.log(e);
-              }
-            );
-          });
-          div.appendChild(del);
-        }
-      },
-      err => {
-        console.log(err);
-      }
-    );
-    container.appendChild(div);
-  }
-  container.scrollTop = 0;
-}
